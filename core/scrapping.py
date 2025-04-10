b6128fa6c9dadd3607c40295d43f1365552e83d2
Konsstantine Belchew <belchev95@icloud.com>
2025-04-10 10:01:59 +0300

    Update scrapping.py

--- core/scrapping.py
+++ core/scrapping.py
@@ -1,16 +1,24 @@
 import requests
 import re
+import pandas as pd
 
 # Channel mapping
 channel_mapping = {
     '#EXTINF:-1 tvg-name="БНТ 1" tvg-logo="https://www.glebul.com/images/tv-logo/bnt-1-hd.png" group-title="ЕФИРНИ" , BNT 1 HD': 'https://www.seir-sanduk.com/?id=hd-bnt-1-hd&pass=&hash=',
 }
 
-# Creating function to m3u8 sniffer
+# Добавяме User-Agent, за да изглеждаме като нормален браузър
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
+
+# Функция за търсене на m3u8 линкове
 def update_links(channel, source_link):
     with requests.Session() as session:
-        response = session.get(source_link)
-        # Регулярен израз, който търси линкове започващи с "https://cdn" и съдържащи "index.m3u8?"
+        # Изпращаме GET заявка с User-Agent
+        response = session.get(source_link, headers=headers)
+        
+        # Търсим m3u8 линк, който започва с https://cdn и съдържа "index.m3u8?"
         match = re.search(r'https://cdn[^\s"]+index\.m3u8\?[^\s"]*', response.text)
         if match:
             m3u_link = match.group(0)
@@ -20,21 +28,21 @@ def update_links(channel, source_link):
             print(f"No m3u link found for {channel}")
             return None
 
-# Use function to sniff channels links in mapping
+# Използваме функцията за да търсим линковете
 data_list = []
 m3u_links = []
 
 for channel, source_link in channel_mapping.items():
     fetched_link = update_links(channel, source_link)
     data_list.append({'Channel': channel, 'SourceLink': source_link, 'LinkToUpdate': fetched_link})
-    if fetched_link:  # If link is fetched, we add it to the m3u_links list
+    if fetched_link:  # Ако линкът е открит, добавяме го в списъка с m3u линкове
         m3u_links.append(f"{channel}\n{fetched_link}")
 
-# Писане на резултатите в .m3u файл
+# Записваме резултатите в .m3u файл
 file_path = 'sources.m3u'
 
-# Clear the file before writing new links
-with open(file_path, 'w') as file:  # 'w' mode will overwrite the file (clear it first)
+# Изчистваме файла преди да запишем новите линкове
+with open(file_path, 'w') as file:  # 'w' режим ще презапише файла
     file.write('#EXTM3U catchup="flussonic" url-tvg="https://github.com/harrygg/EPG/raw/refs/heads/master/all-2days.details.epg.xml.gz"\n')  # Добавяме на първия ред #EXTM3U
     for link in m3u_links:
         file.write(link + '\n')
