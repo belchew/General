b7b7af066dee29cd3d4f42f21741ab071f2fd073
Konsstantine Belchew <belchev95@icloud.com>
2025-04-10 09:38:22 +0300

    Update scrapping.py

--- core/scrapping.py
+++ core/scrapping.py
@@ -13,7 +13,10 @@ channel_mapping = {
 # Creating function to m3u8 sniffer
 def update_links(channel, source_link):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive'
     }
     with requests.Session() as session:
         print(f"Checking {channel} at {source_link}")
