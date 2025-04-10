e567eee44baf5ed0f6ef423203e0d9978f8f9ac9
Konsstantine Belchew <belchev95@icloud.com>
2025-04-10 09:34:45 +0300

    Update scrapping.py

--- core/scrapping.py
+++ core/scrapping.py
@@ -1,4 +1,60 @@
+import os
+import base64
+import re
+import pandas as pd
+import requests
+import subprocess
 
+# Channel mapping
+channel_mapping = {
+    '#EXTINF:-1 tvg-name="БНТ 1" tvg-logo="https://www.glebul.com/images/tv-logo/bnt-1-hd.png" group-title="ЕФИРНИ" , BNT 1 HD': 'https://www.seir-sanduk.com/?id=hd-bnt-1-hd&pass=&hash=',
+}
+
+# Creating function to m3u8 sniffer
+def update_links(channel, source_link):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+    }
+    with requests.Session() as session:
+        print(f"Checking {channel} at {source_link}")
+        response = session.get(source_link, headers=headers)
+        print(f"HTTP Status Code for {channel}: {response.status_code}")
+        
+        if response.status_code == 200:
+            match = re.search(r'https://[^\s"]+\.m3u8(?:\?[^\s"]*)?', response.text)
+            if match:
+                m3u_link = match.group(0)
+                print(f"Fetched m3u link for {channel}: {m3u_link}")
+                return m3u_link
+            else:
+                print(f"No m3u link found for {channel}")
+                return None
+        else:
+            print(f"Failed to retrieve content for {channel}. Status code: {response.status_code}")
+            return None
+
+# Use function to sniff channels links in mapping
+data_list = []
+m3u_links = []
+
+for channel, source_link in channel_mapping.items():
+    fetched_link = update_links(channel, source_link)
+    data_list.append({'Channel': channel, 'SourceLink': source_link, 'LinkToUpdate': fetched_link})
+    if fetched_link:  # If link is fetched, we add it to the m3u_links list
+        m3u_links.append(f"{channel}\n{fetched_link}")
+
+channel_df = pd.DataFrame(data_list)
+
+# Write the fetched m3u links into the sources.m3u file
+file_path = 'sources.m3u'
+
+# Clear the file before writing new links
+with open(file_path, 'w') as file:  # 'w' mode will overwrite the file (clear it first)
+    file.write('#EXTM3U catchup="flussonic" url-tvg="https://github.com/harrygg/EPG/raw/refs/heads/master/all-2days.details.epg.xml.gz"\n')  # Добавяме на първия ред #EXTM3U
+    for link in m3u_links:
+        file.write(link + '\n')
+
+print(f"File {file_path} successfully updated with new links.")
 import os
 import base64
 import re
