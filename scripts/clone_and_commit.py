a3a06e8f9190be1409e53a23584221b809e6f542
Konsstantine Belchew <belchev95@icloud.com>
2025-05-17 16:21:35 +0300

    Update clone_and_commit.py

--- scripts/clone_and_commit.py
+++ scripts/clone_and_commit.py
@@ -5,7 +5,8 @@ import git
 # Път до твоето репозиторио, това ще бъде автоматично във всяка стъпка на GitHub Actions
 repo_path = os.getcwd()  
 
-file_url = 'https://raw.githubusercontent.com/rosendonchev/skandalScraping/refs/heads/main/sources.m3u' 
+file_url = 'https://raw.githubusercontent.com/belchew/connectdirect/refs/heads/main/sources.m3u' 
+#https://raw.githubusercontent.com/rosendonchev/skandalScraping/refs/heads/main/sources.m3u
 
 # Локален път, където ще запишеш файла
 local_filename = 'basic.m3u' 
